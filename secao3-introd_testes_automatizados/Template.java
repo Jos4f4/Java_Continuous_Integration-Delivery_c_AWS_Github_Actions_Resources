9e50f6d739d54c6e7815e36d6e2f561c6ffe8e7d
Josaf4 <josafa.soares2909@gmail.com>
2025-12-27 00:46:35 -0400

    Section 3

---
+++ secao3-introd_testes_automatizados/Template.java
@@ -0,0 +1,8 @@
+// test[System Under Test]_[Condition or State Change]_[Expected Result]
+@DisplayName("Display Name")
+@Test
+void testABCD_when_XYZ_Should() {
+	// Given / Arrange
+	// When / Act
+	// Then / Assert
+}
